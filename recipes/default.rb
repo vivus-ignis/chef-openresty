6a3dacd0e0255aad3164bcf1363bf4250de74be0
Vivus Ignis <mail2ignis@gmail.com>
2012-12-01 12:52:08 +0200

    source_* vars fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,13 +20,13 @@ end
 Chef::Log.debug("// openresty: 3rd-party modules configuration: #{node['openresty']['third_party_modules'].inspect}")
 
 node['openresty']['third_party_modules'].each do |mod_name, mod_params|
-  if mod_params['source']
+  if mod_params['source_url']
 
     Chef::Log.debug("// openresty : going to prepare 3rd-party module #{mod_name}")
 
     remote_file "nginx 3rd-party module: #{mod_name}" do
       path   "#{Chef::Config[:file_cache_path]}/#{::File.basename mod_params['source_url']}"
-      source mod_params['source']
+      source mod_params['source_url']
       
       not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/#{::File.basename mod_params['source_url']}" }
     end
